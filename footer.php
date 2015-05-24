d95b02d2d8de44cf8b312915e950e601c01e86b5
Josh Clark <toastyghost@live.com>
2015-05-24 03:14:30 +0000

    added contact info & social icons to footer

--- footer.php
+++ footer.php
@@ -2,6 +2,16 @@
 
 				<div id="inner-footer" class="wrap cf">
 
+					<p class="contact-info">
+						<strong>Questions? Contact Us:</strong><br>
+						<a href='&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;&#111;&#109;&#101;&#103;&#97;&#99;&#111;&#110;&#118;&#101;&#110;&#116;&#105;&#111;&#110;&#46;&#99;&#111;&#109;'>&#105;&#110;&#102;&#111;&#64;&#111;&#109;&#101;&#103;&#97;&#99;&#111;&#110;&#118;&#101;&#110;&#116;&#105;&#111;&#110;&#46;&#99;&#111;&#109;</a><br>
+						<a href="tel://434-973-2205">434.973.2205</a><br>
+						<br>
+						<a class="social-icon facebook" target="_blank" href="https://facebook.com/omegaconVA">Facebook</a>
+						<a class="social-icon twitter" target="_blank" href="https://twitter.com/OmegaConVA">Twitter</a>
+						<a class="social-icon google" target="_blank" href="https://plus.google.com/+Omegaconvention">Google+</a>
+					</p>
+
 					<nav role="navigation">
 						<?php wp_nav_menu(array(
     					'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
